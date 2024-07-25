ec24deecf92f0dbe64e5b609fd2d9f559b4cdea0
John Estes <john.r.estes@icloud.com>
2024-07-25 00:26:07 -0500

    roled back

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import nodemailer from 'nodemailer';
 import { google } from 'googleapis';
+
 const { OAuth2 } = google.auth;
 
 dotenv.config();
@@ -18,7 +19,6 @@ const port = 3000;
 
 const secretKey = process.env.SECRET_KEY;
 const hashedPassword = process.env.HASHED_PASSWORD;
-
 const version = '1.5.0';
 
 const corsOptions = {
@@ -28,7 +28,6 @@ const corsOptions = {
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
 };
-
 
 app.use(cors(corsOptions));
 app.use(rateLimitMiddleware);
@@ -41,47 +40,43 @@ const users = [
       username: 'yolanda',
       password: hashedPassword,
     },
-  ];
-  
+];
 
 app.get('/', (req, res) => {
-    // set response content    
-        res.send(`<html>
-                    <body>
-                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: Server is Running.</h1>
-                        <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
-                            <img src="https://picsum.photos/400/400?random=1">
-                        </div>
-                    </body>
-                   </html>`);
- 
-  console.log(`[Version ${version}]: New request => http://${hostname}:${port}`+req.url);
-
-})
+    res.send(`<html>
+                <body>
+                    <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: Server is Running.</h1>
+                    <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
+                        <img src="https://picsum.photos/400/400?random=1">
+                    </div>
+                </body>
+               </html>`);
+    console.log(`[Version ${version}]: New request => http://${hostname}:${port}` + req.url);
+});
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-  
-    const user = users.find(u => u.username === username);
-  
-    if (user && bcrypt.compareSync(password, user.password)) {
-      console.log('Password comparison successful');
-      const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: '30d' });
-      res.json({ token });
-    } else {
-      console.log('Password comparison failed');
-      res.status(401).json({ error: 'Invalid credentials' });
-    }
-  });
 
-  app.get('/protected-route', authenticateToken, (req, res) => {
+    const user = users.find(u => u.username === username);
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+        console.log('Password comparison successful');
+        const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: '30d' });
+        res.json({ token });
+    } else {
+        console.log('Password comparison failed');
+        res.status(401).json({ error: 'Invalid credentials' });
+    }
+});
+
+app.get('/protected-route', authenticateToken, (req, res) => {
     res.json({ message: 'This is a protected route!' });
-  });
+});
 
 app.get('/api/victron/data', async (req, res) => {
     try {
         const data = await fetchAllData();
-        res.json(data); // send the data as a JSON response
+        res.json(data);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
@@ -92,55 +87,51 @@ app.listen(port, () => {
     console.log(`[Version ${version}]: Server running at http://${hostname}:${port}/`);
 });
 
-var token, idUser, idSite;
 async function fetchAllData() {
-    // Access environment variables
-
     const usernameVic = process.env.USERNAME;
     const passwordVic = process.env.PASSWORD;
-    var data;
+    let token, idUser, idSite, data;
 
     try {
         await fetchData();
-        return data; // return the result (dataArray) from fetchData
+        return data;
     } catch (error) {
         console.error(error);
         throw error;
     }
 
     async function fetchData() {
-        if (token == null ){
+        if (!token) {
             try {
                 await get_login_token();
                 await get_installations();
-                data = await get_Chart(); 
+                data = await get_Chart();
             } catch (error) {
                 console.error(error);
                 throw error;
             }
         } else {
             try {
-                data = await get_Chart(); 
+                data = await get_Chart();
             } catch (error) {
                 token = null;
                 console.error(error);
                 throw error;
             }
         }
-
     }
 
     async function get_login_token() {
-        var myHeaders = new Headers();
+        const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
 
-        var raw = JSON.stringify({
+        const raw = JSON.stringify({
             "username": usernameVic,
             "password": passwordVic,
             "remember_me": "true"
         });
 
-        var requestOptions = {
+        const requestOptions = {
             method: 'POST',
             headers: myHeaders,
             body: raw,
@@ -149,21 +140,20 @@ async function fetchAllData() {
 
         try {
             const response = await fetch("https://vrmapi.victronenergy.com/v2/auth/login/", requestOptions);
-            const result = await response.text();
-            const data = JSON.parse(result); // result is a JSON string
-            token = data.token
-            idUser = data.idUser
+            const result = await response.json();
+            token = result.token;
+            idUser = result.idUser;
         } catch (error) {
             console.log('error', error);
-            throw error; // Rethrow the error to handle it outside this function if needed
+            throw error;
         }
     }
 
     async function get_installations() {
-        console.log("Get Installation #")
+        console.log("Get Installation #");
         const headers = { 'X-Authorization': `Bearer ${token}` };
 
-        var requestOptions = {
+        const requestOptions = {
             method: 'GET',
             headers: headers,
             redirect: 'follow'
@@ -171,57 +161,36 @@ async function fetchAllData() {
 
         try {
             const response = await fetch(`https://vrmapi.victronenergy.com/v2/users/${idUser}/installations`, requestOptions);
-            const result = await response.text();
-            const data = JSON.parse(result); // result is a JSON string
-            idSite = data.records[0].idSite
+            const result = await response.json();
+            idSite = result.records[0].idSite;
         } catch (error) {
             console.log('error', error);
-            throw error; // Rethrow the error to handle it outside this function if needed
+            throw error;
         }
-    }
-
-    async function get_Overall_Stats() {
-        console.log("Get Overall Stats")
-        const headers = { 'X-Authorization': `Bearer ${token}` };
-        var stat_data;
-        var requestOptions = {
-            method: 'GET',
-            headers: headers,
-            redirect: 'follow'
-        };
-
-        try {
-            const response = await fetch(`https://vrmapi.victronenergy.com/v2/installations/${idSite}/overallstats`, requestOptions);
-            const result = await response.text();
-            stat_data = JSON.parse(result); // result is a JSON string
-        } catch (error) {
-            console.log('error', error);
-            throw error; // Rethrow the error to handle it outside this function if needed
-        }
-        console.log(JSON.stringify(stat_data, null, 2));
     }
 
     async function get_Chart() {
-        console.log("Get Chart")
+        console.log("Get Chart");
         const headers = { 'X-Authorization': `Bearer ${token}` };
-        var requestOptions = {
+
+        const requestOptions = {
             method: 'GET',
             headers: headers,
             redirect: 'follow'
         };
-    
+
         try {
             const response = await fetch(`https://vrmapi.victronenergy.com/v2/installations/${idSite}/diagnostics`, requestOptions);
-            const result = await response.text();
-            const data = JSON.parse(result); // result is a JSON string
-            if (!data.success) {
+            const result = await response.json();
+
+            if (!result.success) {
                 throw new Error('The returned response did not indicate success.');
             }
-    
-            if (!data.records?.length) {
+
+            if (!result.records?.length) {
                 throw new Error('The response data array is either missing or empty.');
             }
-    
+
             const desiredAttributes = new Set([
                 81, // Voltage
                 49, // Current
@@ -230,8 +199,8 @@ async function fetchAllData() {
                 96, // Yesterday's Daily Yield
                 442, // PV Power
             ]);
-    
-            let dataArray = data.records
+
+            const dataArray = result.records
                 .filter(record => desiredAttributes.has(record.idDataAttribute))
                 .map(record => ({
                     idDataAttribute: record.idDataAttribute,
@@ -239,37 +208,36 @@ async function fetchAllData() {
                     formattedValue: record.formattedValue,
                     timestamp: record.timestamp
                 }));
+
             return dataArray;
         } catch (error) {
             console.log('error', error);
-            throw error; // Rethrow the error to handle it outside this function if needed
+            throw error;
         }
     }
-    
 }
 
 function authenticateToken(req, res, next) {
     const token = req.header('Authorization');
-  
-    if (!token) {
-      return res.status(401).json({ error: 'Unauthorized - Token not provided' });
-    }
-  
-    // Extract token without the 'Bearer ' prefix
-    const tokenWithoutBearer = token.replace('Bearer ', '');
-  
-    jwt.verify(tokenWithoutBearer, secretKey, (err, user) => {
-      if (err) {
-        console.error('Token verification error:', err);
-        return res.status(403).json({ error: 'Token is not valid or expired' });
-      }
-  
-      req.user = user;
-      next();
-    });
-  }
 
-//GMail Integration
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized - Token not provided' });
+    }
+
+    const tokenWithoutBearer = token.replace('Bearer ', '');
+
+    jwt.verify(tokenWithoutBearer, secretKey, (err, user) => {
+        if (err) {
+            console.error('Token verification error:', err);
+            return res.status(403).json({ error: 'Token is not valid or expired' });
+        }
+
+        req.user = user;
+        next();
+    });
+}
+
+// Gmail Integration
 
 const oauth2Client = new OAuth2(
     process.env.CLIENT_ID,
@@ -293,8 +261,7 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-app.post('/send-email', (req, res) => {
-
+app.post('/send-email', async (req, res) => {
     const mailOptions = {
         from: req.body.email,
         to: process.env.EMAIL, // Replace with your receiving email
@@ -311,12 +278,14 @@ app.post('/send-email', (req, res) => {
         `
     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error('Error sending email:', error);
-            return res.status(500).send(error.toString());
-        }
+    try {
+        const accessToken = await oauth2Client.getAccessToken();
+        transporter.transporter.auth.accessToken = accessToken.token;
+        const info = await transporter.sendMail(mailOptions);
         console.log('Email sent:', info.response);
         res.status(200).send('Email sent: ' + info.response);
-    });
-});  
+    } catch (error) {
+        console.error('Error sending email:', error);
+        res.status(500).send(error.toString());
+    }
+});
