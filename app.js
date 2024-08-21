67749b67572d8fe9af9349a058c4cab4b9119c2b
John Estes <john.r.estes@icloud.com>
2024-08-21 08:07:00 -0500

    added storage of total day solar

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 import fs from 'fs'
+import cron from 'node-cron';
+import moment from 'moment-timezone';
 
 dotenv.config();
 
@@ -141,6 +143,7 @@ async function fetchAllData() {
             const result = await response.json();
             token = result.token;
             idUser = result.idUser;
+            console
         } catch (error) {
             console.log('error', error);
             throw error;
@@ -161,6 +164,7 @@ async function fetchAllData() {
             const response = await fetch(`https://vrmapi.victronenergy.com/v2/users/${idUser}/installations`, requestOptions);
             const result = await response.json();
             idSite = result.records[0].idSite;
+            console.log(idSite)
         } catch (error) {
             console.log('error', error);
             throw error;
@@ -329,37 +333,46 @@ async function logoutGrowatt() {
 }
 
 app.get('/api/growattData', authenticateToken, async (req, res) => {
-  try {
-    lastRequestTime = Date.now();
-    await loginGrowatt();
-
-    let getAllPlantData = await growatt.getAllPlantData({});
-    
-    // Extract required data
-    const yolandaData = getAllPlantData['4466']['devices']['UKDFBHG0GX']['statusData'];
-    const casaMJData1 = getAllPlantData['25328']['devices']['XSK0CKS058']['statusData'];
-    const casaMJData2 = getAllPlantData['25328']['devices']['XSK0CKS03A']['statusData'];
-    const yolandaDataTotal = getAllPlantData['4466']['devices']['UKDFBHG0GX']['totalData'];
-    const casaMJData1Total = getAllPlantData['25328']['devices']['XSK0CKS058']['totalData'];
-    const casaMJData2Total = getAllPlantData['25328']['devices']['XSK0CKS03A']['totalData'];
-    const weatherDataYolanda = getAllPlantData['4466']['weather']['data']['HeWeather6'][0];
-    const weatherDataCasaMJ = getAllPlantData['25328']['weather']['data']['HeWeather6'][0];
-    
-    res.json({
-      yolandaData,
-      casaMJData1,
-      casaMJData2,
-      yolandaDataTotal,
-      casaMJData1Total,
-      casaMJData2Total,
-      weatherDataYolanda,
-      weatherDataCasaMJ
-    });
-  } catch (e) {
-    console.error(e);
-    res.status(500).send('Internal Server Error');
-  }
+    try {
+        lastRequestTime = Date.now();
+        const data = await getGrowattData();
+        res.json(data);
+    } catch (e) {
+        res.status(500).send('Internal Server Error');
+    }
 });
+
+async function getGrowattData() {
+    try {
+        await loginGrowatt();
+
+        let getAllPlantData = await growatt.getAllPlantData({});
+
+        // Extract required data
+        const yolandaData = getAllPlantData['4466']['devices']['UKDFBHG0GX']['statusData'];
+        const casaMJData1 = getAllPlantData['25328']['devices']['XSK0CKS058']['statusData'];
+        const casaMJData2 = getAllPlantData['25328']['devices']['XSK0CKS03A']['statusData'];
+        const yolandaDataTotal = getAllPlantData['4466']['devices']['UKDFBHG0GX']['totalData'];
+        const casaMJData1Total = getAllPlantData['25328']['devices']['XSK0CKS058']['totalData'];
+        const casaMJData2Total = getAllPlantData['25328']['devices']['XSK0CKS03A']['totalData'];
+        const weatherDataYolanda = getAllPlantData['4466']['weather']['data']['HeWeather6'][0];
+        const weatherDataCasaMJ = getAllPlantData['25328']['weather']['data']['HeWeather6'][0];
+
+        return {
+            yolandaData,
+            casaMJData1,
+            casaMJData2,
+            yolandaDataTotal,
+            casaMJData1Total,
+            casaMJData2Total,
+            weatherDataYolanda,
+            weatherDataCasaMJ
+        };
+    } catch (e) {
+        console.error('Error fetching Growatt data:', e);
+        throw e;
+    }
+}
 
 // Periodically check if logout is needed
 setInterval(async () => {
@@ -367,3 +380,88 @@ setInterval(async () => {
     await logoutGrowatt();
   }
 }, 30 * 1000); // Check every 30 seconds
+
+
+
+// Function to write data to the file
+const writeDataToFile = async () => {
+    const timeZone = 'America/Cancun';
+    const currentDate = moment().tz(timeZone).format('YYYY-MM-DD');
+    
+    try {
+        const data1 = await fetchAllData();
+        const data2 = await getGrowattData();
+
+        const towerDayTotal = data1[4]['formattedValue'] || 'Data not available';
+        const yolandaDayTotal = data2.yolandaDataTotal.epvToday || 'Data not available';
+        const casa1DayTotal = data2.casaMJData1Total.epvToday || 'Data not available';
+        const casa2DayTotal = data2.casaMJData2Total.epvToday || 'Data not available';
+
+        // Prepare the data to write to the file
+        const data = `Date: ${currentDate}: [${towerDayTotal}, ${yolandaDayTotal}, ${casa1DayTotal}, ${casa2DayTotal}]\n`;
+
+        // Write the values to a file
+        fs.appendFile('solar_data.txt', data, (err) => {
+            if (err) {
+                console.error('Error writing to file:', err);
+            } else {
+                console.log('Data written to file successfully.');
+            }
+        });
+    } catch (error) {
+        console.error('Error running methods:', error);
+    }
+};
+
+// Schedule the task to run at 7:30 PM Cancun Time
+cron.schedule('30 19 * * *', async () => {
+    await writeDataToFile();
+}, {
+    timezone: "America/Cancun"
+});
+
+// Function to get the last entry from the file
+const getLastEntry = (filePath) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+
+            // Split the file data by newlines to get an array of lines
+            const lines = data.trim().split('\n');
+
+            // Get the last line (most recent entry)
+            const lastLine = lines[lines.length - 1];
+
+            resolve(lastLine);
+        });
+    });
+};
+
+// Function to read the last entry from the file
+const readSolarTotal = async () => {
+    try {
+        const lastEntry = await getLastEntry('solar_data.txt');
+        return lastEntry;
+    } catch (error) {
+        console.error('Error reading file:', error);
+    }
+};
+
+// Usage example: Run the task immediately for testing and then get the last entry
+// (async () => {
+//     await writeDataToFile(); // Manually run the task
+//     const lastEntry = await readSolarTotal(); // Test read
+//     console.log('Last Entry:', lastEntry);
+// })();
+
+// API endpoint to return the last entry from the file
+app.get('/api/lastEntry', authenticateToken, async (req, res) => {
+    try {
+        const lastEntry = await getLastEntry('solar_data.txt');
+        res.json({ lastEntry });
+    } catch (error) {
+        res.status(500).send('Error reading file');
+    }
+});
