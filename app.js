f2914cb7261e5c117638113dfc2abe2a759e044e
John Estes <john.r.estes@icloud.com>
2026-06-14 18:35:31 -0500

    Confirm v2 server and frontend integration

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const allowedOrigins = [
   'https://johnetravels.com',
   'https://www.johnetravels.com',
   'http://localhost:8080',
-  'http://localhost:5500'
+  'http://127.0.0.1:8080',
+  'http://localhost:5500',
+  'http://127.0.0.1:5500'
 ];
 
 app.use(cors({
